bd3f335db8551fb22680472fe79c73cf74b233b3
LukzST <lucaseduarte6@gmail.com>
2026-03-09 16:05:12 -0300

    web10

---
+++ Public/JS/scripts.js
@@ -0,0 +1,24 @@
+function itch() {
+    window.open('https://lukzxdd.itch.io/light/purchase?popup=1', 'Popup', 'width=500,height=400,scrollbars=no');
+}
+
+function support() {
+    window.open('mailto:contatosadberry@gmail.com', 'Popup', 'width=500,height=400,scrollbars=no');
+}
+
+const menu = document.getElementById('menuMobile');
+
+menu.addEventListener('click', function(event) {
+    this.classList.toggle('active');
+});
+
+const links = document.querySelectorAll('.text_header');
+
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.stopPropagation(); 
+        
+        menu.classList.remove('active');
+        
+    });
+});
