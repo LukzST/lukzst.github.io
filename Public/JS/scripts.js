ac0e11c375837eb6e90a1293b77c9875ca1a32b8
LukzST <lucaseduarte6@gmail.com>
2026-03-09 16:19:28 -0300

    web10

--- Public/JS/scripts.js
+++ Public/JS/scripts.js
@@ -6,6 +6,10 @@ function support() {
     window.open('mailto:contatosadberry@gmail.com', 'Popup', 'width=500,height=400,scrollbars=no');
 }
 
+function soundtrack() {
+    window.open('https://www.youtube.com/playlist?list=PLstaXaIuDSEidUQAif2ptoqswAv8qTJ6y', 'Popup', 'width=800,height=600,scrollbars=no');
+}
+
 const menu = document.getElementById('menuMobile');
 
 menu.addEventListener('click', function(event) {
