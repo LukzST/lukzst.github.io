c05532cd723795820e04cc1fe61879920f3be3a1
LukzST <lucaseduarte6@gmail.com>
2026-03-09 22:19:06 -0300

    fix bugs4

--- demo/script.js
+++ demo/script.js
@@ -1,40 +1,16 @@
-const repoOwner = 'lukzst';
-        const repoName = 'light';
-        const targetPath = 'FINAL';
+const menu = document.getElementById('menuMobile');
 
-        async function fetchVersions() {
-            const container = document.getElementById('version-container');
-            
-            try {
-                const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${targetPath}`);
-                if (!response.ok) throw new Error();
-                
-                const data = await response.json();
-                container.innerHTML = '';
+menu.addEventListener('click', function(event) {
+    this.classList.toggle('active');
+});
 
-                const folders = data.filter(item => item.type === 'dir' && item.name !== 'BETA_V1.0');
+const links = document.querySelectorAll('.text_header');
 
-                folders.forEach(folder => {
-                    const card = document.createElement('a');
-                    card.href = `https://github.com/${repoOwner}/${repoName}/tree/main/${targetPath}/${folder.name}`;
-                    card.target = "_blank";
-                    card.className = 'version-card';
-                    
-                    card.innerHTML = `
-                        <h2>${folder.name.toUpperCase()}</h2>
-                        <p>Final build located in the repository. Click to access the files for this version.</p>
-                        <span style="color: var(--primary-color); font-weight: bold;">EXPLORE →</span>
-                    `;
-                    
-                    container.appendChild(card);
-                });
-            } catch (e) {
-                container.innerHTML = '<p class="about-text">Erro ao carregar versões ou pasta vazia.</p>';
-            }
-        }
-
-        window.onload = fetchVersions;
-
-        window.addEventListener("beforeunload", function () {
-  document.body.classList.add("animate-out");
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.stopPropagation(); 
+        
+        menu.classList.remove('active');
+        
+    });
 });
--- updates/script.js
+++ updates/script.js
@@ -1,3 +1,20 @@
 window.addEventListener("beforeunload", function () {
   document.body.classList.add("animate-out");
 });
+
+const menu = document.getElementById('menuMobile');
+
+menu.addEventListener('click', function(event) {
+    this.classList.toggle('active');
+});
+
+const links = document.querySelectorAll('.text_header');
+
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.stopPropagation(); 
+        
+        menu.classList.remove('active');
+        
+    });
+});
