bf66a7365d3150ec09086aa33c3ad2753a0fbc6c
LukzST <lucaseduarte6@gmail.com>
2026-05-05 13:16:56 -0300

    downloadAlphaFix

--- src/pages/games/INSOMNIA.jsx
+++ src/pages/games/INSOMNIA.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import insomniaLogo from "../../assets/images/i2.png";
 import trailer1 from "../../assets/videos/trailer.mp4";
 import trailer2 from "../../assets/videos/trailer2.mp4";
@@ -6,6 +7,35 @@ import useExternalStyle from "../../hooks/useExternalStyle";
 
 export default function GamesINSOMNIA() {
     useExternalStyle('insomnia.css');
+
+    const [ downloadUrl, setDownloadUrl ] = useState('https://media.githubusercontent.com/media/LuxJson/INSOMNIA/refs/heads/main/files/0.0.2/insomnia_v002a_setup.exe')
+    
+     useEffect(() => {
+        async function fetchLatestVersion() {
+            try {
+                const response = await fetch('https://github.com');
+                const data = await response.json();
+
+                if (Array.isArray(data)) {
+                    const folders = data
+                        .filter(item => item.type === 'dir')
+                        .sort((a, b) => b.name.localeCompare(a.name, undefined, { numeric: true }));
+
+                    if (folders.length > 0) {
+                        const latestVersion = folders[0].name;
+                        const fileVersion = latestVersion.replace(/\./g, ''); 
+                        
+                        const newUrl = `https://githubusercontent.com{latestVersion}/insomnia_v${fileVersion}a_setup.exe`;
+                        setDownloadUrl(newUrl);
+                    }
+                }
+            } catch (error) {
+                console.error("Erro ao buscar versão recente:", error);
+            }
+        }
+
+        fetchLatestVersion();
+    }, []);
 
     const hoje = new Date();
     const dataStoryTrailer = new Date('2026-10-31T12:00:00-03:00');
@@ -29,7 +59,7 @@ export default function GamesINSOMNIA() {
                     <a href="https://UnburiedPixels.itch.io/insomnia" target="_blank" className="link" rel="noreferrer">
                         <div className="border">itch.io</div>
                     </a>
-                    <a href="https://media.githubusercontent.com/media/LuxJson/INSOMNIA/refs/heads/main/files/0.0.2/insomnia_v002a_setup.exe" target="_blank" className="link-4" rel="noreferrer">
+                    <a href={downloadUrl} target="_blank" className="link-4" rel="noreferrer">
                         <div className="border2">Download Alpha</div>
                     </a>
                     <Link to="/games/insomnia/404.html" className="link">
